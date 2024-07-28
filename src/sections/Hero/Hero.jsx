fb39ae56eb634c892ad89851b84495300d19ef3e
Adam Counihan <137104673+adamlcounihan@users.noreply.github.com>
2024-07-29 00:35:42 +0100

    style: add CSS styling for hero content

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -4,26 +4,29 @@ import githubIcon from '../../assets/github.svg'
 import linkedinIcon from '../../assets/linkedin.svg'
 
 function Hero() {
-    return <section id="hero" className={styles.container}>
-        <div className={styles.colorModeContainer}>
-            <img className={styles.hero} src={heroImg} alt="Profile picture of Adam Counihan"/>
-        </div>
-        <div className={styles.info}>
-            <h1>Adam <br/> Counihan</h1>
-            <h2>Computer Science Student</h2>
-            <span>
-                <a href="https://github.com/adamlcounihan/" target="_blank">
-                    <img src={githubIcon} alt="Github icon"/>
-                </a>
-            </span>
-            <span>
-                <a href="https://linkedin.com/in/adamlcounihan/" target="_blank">
-                    <img src={linkedinIcon} alt="Linkedin icon"/>
-                </a>
-            </span>
-            <p>Studying at University of Kent (B.Sc.)</p>
-        </div>
-    </section>;
+    return (
+        <section id="hero" className={styles.container}>
+            <div>
+                <img className={styles.hero} src={heroImg} alt="Profile picture of Adam Counihan"/>
+            </div>
+            <div className={styles.info}>
+                <h1 className={styles.title}>Adam Counihan</h1>
+                <p className={styles.description}>
+                    Studying Computer Science at
+                    University of Kent (B.Sc.)
+                </p>
+                <div className={styles.socialMedia}>
+                    <a href="https://github.com/adamlcounihan/" target="_blank">
+                        <img src={githubIcon} alt="Github icon"/>
+                    </a>
+                    <a href="https://linkedin.com/in/adamlcounihan/" target="_blank">
+                        <img src={linkedinIcon} alt="Linkedin icon"/>
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
 }
+
 
 export default Hero;
