ce6b620cfe8c07905780916ac0bd07173d05c595
Adam Counihan <137104673+adamlcounihan@users.noreply.github.com>
2024-08-04 23:02:43 +0100

    feat: add skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Hero from "./components/Hero/Hero.jsx";
 import Projects from "./components/Projects/Projects.jsx";
+import Skills from "./components/Skills/Skills.jsx";
 
 function App() {
   return<>
     <Hero/>
     <Projects/>
+    <Skills/>
   </>
 }
 
