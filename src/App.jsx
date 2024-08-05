eae8e79857e980b9af2f1bf48a077df1abdab9c7
Adam Counihan <137104673+adamlcounihan@users.noreply.github.com>
2024-08-05 21:46:38 +0100

    feat: add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Hero from "./components/Hero/Hero.jsx";
 import Projects from "./components/Projects/Projects.jsx";
 import Skills from "./components/Skills/Skills.jsx";
+import Footer from "./components/Footer/Footer.jsx"
 
 function App() {
   return<>
     <Hero/>
     <Projects/>
     <Skills/>
+    <Footer/>
   </>
 }
 
