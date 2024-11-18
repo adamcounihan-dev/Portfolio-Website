4bd142a36b168470f76faa1736c597a3cabe69a2
Adam Counihan <137104673+adamlcounihan@users.noreply.github.com>
2024-11-18 01:39:41 +0000

    style: improve skills section

--- pages/Skills/Skills.jsx
+++ pages/Skills/Skills.jsx
@@ -3,29 +3,27 @@ import styles from './SkillsStyles.module.css';
 const skills = {
     Languages: ['HTML', 'CSS', 'JavaScript', 'Java', 'Python', 'PHP', 'C', 'C#'],
     'Database Management': ['MySQL'],
-    'Frameworks and Libraries': ['ReactJS', 'Node.js', 'Vite'],
+    'Frameworks and Libraries': ['ReactJS', 'Node.js', 'Vite', 'CodeIgniter'],
     'Tools and Platforms': ['Unix', 'Figma', 'Adobe XD', 'GitHub', 'Docker']
 };
 
 function Skills() {
     return (
-        <section id="skills" className={styles.skillsContainer}>
+        <section id="skills" className={styles.skills}>
             <h1 className={styles.title}>Skills</h1>
-            <div className={styles.gridWrapper}>
-                <div className={styles.gridContainer}>
-                    {Object.entries(skills).map(([category, items]) => (
-                        <div key={category} className={styles.gridCategory}>
-                            <h2 className={styles.categoryTitle}>{category}</h2>
-                            <div className={styles.gridItems}>
-                                {items.map((skill, index) => (
-                                    <div key={index} className={styles.gridItem}>
-                                        {skill}
-                                    </div>
-                                ))}
-                            </div>
+            <div className={styles.skillsGrid}>
+                {Object.entries(skills).map(([category, items]) => (
+                    <div key={category} className={styles.skillCategory}>
+                        <h3 className={styles.categoryTitle}>{category}</h3>
+                        <div className={styles.skillsContainer}>
+                            {items.map((skill, index) => (
+                                <div key={index} className={styles.skillItem}>
+                                    {skill}
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
         </section>
     );
