5066071d442c70593df4d40da52ba51792848c6f
Adam Counihan <137104673+adamlcounihan@users.noreply.github.com>
2024-12-07 19:16:12 +0000

    style: move skills to about page

--- pages/About/About.jsx
+++ pages/About/About.jsx
@@ -1,28 +1,60 @@
 import styles from './AboutStyles.module.css';
 import heroImg from '/src/assets/hero-img.webp';
+import { Link } from 'react-router-dom';
+
+const skills = {
+    Languages: ['HTML', 'CSS', 'JavaScript', 'Java', 'Python', 'PHP', 'C', 'C#'],
+    'Database Management': ['MySQL'],
+    'Frameworks and Libraries': ['ReactJS', 'Node.js', 'Vite', 'CodeIgniter'],
+    'Tools and Platforms': ['Unix', 'Figma', 'Adobe XD', 'GitHub', 'Docker']
+};
 
 function About() {
     return (
         <section id="about" className={styles.about}>
             <h1 className={styles.title}>About Me</h1>
-            <div className={styles.aboutContent}>
+            <div className={styles.contentContainer}>
                 <div className={styles.profileContainer}>
-                    <img src={heroImg} alt="Adam Counihan" className={styles.profileImage}/>
+                    <img
+                        src={heroImg}
+                        alt="Adam Counihan"
+                        className={styles.profileImage}
+                    />
+                    <div className={styles.buttonsContainer}>
+                        <Link to="/projects" className={styles.button}>
+                            See My Projects
+                        </Link>
+                    </div>
                 </div>
-                <p className={styles.text}>
-                    Hi, I’m Adam Counihan – a Computer Science student at the University of Kent with a passion for
-                    technology and creativity. <br/>
-                    I’m driven by a love for software development and design, whether it’s building visually appealing
-                    web interfaces, creating interactive applications, or crafting efficient algorithms.
-                    I aim to blend technical precision with creativity to develop meaningful user
-                    experiences. <br/><br/>
-                    Whether you’re interested in collaborating, exploring new ideas, or just making connections in the
-                    field, I’d love to hear from you.
-                </p>
+                <div className={styles.bioContainer}>
+                    <p className={styles.text}>
+                        Hi, I’m Adam Counihan – a Computer Science student at the University
+                        of Kent with a passion for technology and creativity. <br /><br />
+                        I’m driven by a love for software development and design, whether it’s
+                        building visually appealing web interfaces, creating interactive
+                        applications, or crafting efficient algorithms. I aim to blend
+                        technical precision with creativity to develop meaningful user
+                        experiences. <br /><br />
+                        Whether you’re interested in collaborating, exploring new ideas, or
+                        just making connections in the field, I’d love to hear from you.
+                    </p>
+                </div>
+
+                <div className={styles.skillsContainer}>
+                    {Object.keys(skills).map((category) => (
+                        <div className={styles.skillCategory} key={category}>
+                            <h3 className={styles.skillCategoryTitle}>{category}</h3>
+                            <ul className={styles.skillList}>
+                                {skills[category].map((skill, index) => (
+                                    <li key={index} className={styles.skillItem}>{skill}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     );
 }
 
 export default About;
-
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar/Navbar.jsx";
 import Home from "../pages/Home/Home.jsx";
 import About from "../pages/About/About.jsx";
 import Projects from "../pages/Projects/Projects.jsx";
-import Skills from "../pages/Skills/Skills.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
-            <Route path="/skills" element={<Skills />} />
         </Routes>
 
         <Footer />
