28913db94f7c75d2a5fa4f60ea7d700fd523e344
Adam Counihan <137104673+adamlcounihan@users.noreply.github.com>
2024-12-28 23:14:20 +0000

    feat: add new certification

--- pages/About/About.jsx
+++ pages/About/About.jsx
@@ -49,6 +49,7 @@ const skills = {
 };
 
 const certifications = [
+    { title: 'HSBC - Digital Business Services Job Simulation', issuer: 'Forage', date: 'Issued Dec 2024', link: 'https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/jCtDBF7kyfRqQpxp6/eLtRmdoYbFE5oHAQj_jCtDBF7kyfRqQpxp6_9hWHmvXoboTYLbFNR_1735413606428_completion_certificate.pdf' },
     { title: 'Electronic Arts - Software Engineering Job Simulation', issuer: 'Forage', date: 'Issued Dec 2024', link: 'https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/j43dGscQHtJJ57N54/a77WE3de8qrxWferQ_j43dGscQHtJJ57N54_9hWHmvXoboTYLbFNR_1733939465854_completion_certificate.pdf' },
     { title: 'IEUK 2024: Technology', issuer: 'Bright Network', date: 'Issued Jun 2024', link: 'https://www.brightnetwork.co.uk/certificates/ieuk-2024-technology_yubfakdv69wuah/' },
 ];
